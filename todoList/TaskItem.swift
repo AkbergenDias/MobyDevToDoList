b6feb29da8032cf66f8f6b63f365ae6ac1ebb32a
Olzhas Akhmetov <olzhas@192-168-5-87.local>
2024-09-22 21:22:03 +0500

    first commit

---
+++ todoList/TaskItem.swift
@@ -0,0 +1,13 @@
+//
+//  TaskItem.swift
+//  todoList
+//
+//  Created by Olzhas Akhmetov on 22.09.2024.
+//
+
+import Foundation
+
+struct TaskItem: Codable {
+    var name: String
+    var isComplete: Bool
+}
